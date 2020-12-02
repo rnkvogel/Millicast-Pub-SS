37809ebaeccc0d8eeaae0590ee1163ea664418a5
rnkvogel <rvogel@influxis.com>
2020-12-02 07:48:59 -0800

    Update publisher.js

--- screen/js/publisher.js
+++ screen/js/publisher.js
@@ -446,12 +446,13 @@ function getMedia() {
           echoCancellation: false
         }
       }
-     let constraints = {audio: a,
-       video: {
-          width:  { min: 640, max: 1920, ideal: 1280 },
-          height: { min: 480, max: 1080, ideal: 720 },
-          frameRate: { min: 10, max: 60, ideal: 30 },
-        }
+     let constraints = {audio: true,
+       video: true
+                        //{
+         // width:  { min: 640, max: 1920, ideal: 1280 },
+         // height: { min: 480, max: 1080, ideal: 720 },
+         // frameRate: { min: 10, max: 60, ideal: 30 },
+       // }
 
 
       };
